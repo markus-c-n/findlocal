1e9b9b9946d57d335bf7e0821401f9f61316ef9a
Markus Nowak <markusnowak99@gmail.com>
2023-09-08 17:31:06 +0200

    FE store show

--- app/javascript/controllers/store_show_controller.js
+++ app/javascript/controllers/store_show_controller.js
@@ -13,10 +13,18 @@ export default class extends Controller {
   showAdminList() {
     this.adminListTarget.style.display = "block"
     this.publicListTarget.style.display = "none"
+    // Add "active" class to the Admin button
+    this.element.querySelector("[data-action='click->store-show#showAdminList']").classList.add("active");
+    // Remove "active" class from the Preview button
+    this.element.querySelector("[data-action='click->store-show#showPublicList']").classList.remove("active");
   }
 
   showPublicList() {
     this.adminListTarget.style.display = "none"
     this.publicListTarget.style.display = "block"
+    // Add "active" class to the Preview button
+    this.element.querySelector("[data-action='click->store-show#showPublicList']").classList.add("active");
+    // Remove "active" class from the Admin button
+    this.element.querySelector("[data-action='click->store-show#showAdminList']").classList.remove("active");
   }
 }
